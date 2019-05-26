ee7f47c7426ef060617a1061d27f1fe2a2287a09
shimura.atsushi <atsushi.shimura.j.a.n.n.s.f.t.z.n@gmail.com>
2019-05-26 11:10:25 +0900

    head.c

--- head.c
+++ head.c
@@ -1,25 +1,52 @@
 # include <stdio.h>
 # include <stdlib.h>
 
+#define _GNU_SOURCE
+#include <getopt.h>
+
 static void do_head(FILE *f, long nlines);
+
+#define DEFAULT_N_LINES 10
+
+static struct option longopts[] = {
+  {"lines", required_argument, NULL, 'n'},
+  {"help", no_argument, NULL, 'h'},
+  {0, 0, 0, 0}
+};
 
 int
 main(int argc, char *argv[])
 {
-  if(argc != 2){
-    fprintf(stderr, "Usage: %s n\n", argv[0]);
-    exit(1);
+
+  int opt;
+  int nlines = DEFAULT_N_LINES;
+   
+  while((opt = getopt_long(argc, argv, "n", longopts, NULL)) != -1) {
+    switch(opt) {
+    case 'n': 
+      nlines = atol(optarg);
+      break;
+    case 'h': 
+      fprintf(stdout, "Usage: %s [-n LINES][FILE ...]\n", argv[0]);
+      exit(0);
+    case '?': 
+      fprintf(stdout, "Usage: %s [-n LINES][FILE ...]\n", argv[0]);
+      exit(1);
+    }
+  }
+  if(optind == argc) {
+    do_head(stdin, nlines);
   } else {
-    int i;
+    int i; 
     
-    for(i = 2; i < argc ; i++){
+    for(i = optind; i < argc; i++){
       FILE *f;
       f = fopen(argv[i], "r");
-      if(!f){
+      if(!f) {
         perror(argv[i]);
-        exit(1); 
-      }
-      do_head(stdin, atol(argv[1]));
+        exit(1);
+      } 
+      do_head(f, nlines);
       fclose(f);
     }
   }
